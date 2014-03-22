bbe4b20adc594e55a378070a735485796640c5fc
Thomas Debesse <dev@illwieckz.net>
2014-03-22 23:18:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+EpsonEPL Debian Installer
+=========================
+
+This tool download epsonepl driver from sourceforge then compile it and build a Debian package using checkinstall.
+
+Author: Thomas DEBESSE <dev@illwieckz.net>
+License: [Expat](LICENSE.md).
---
+++ build.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+# AUTHOR: Thomas DEBESSE <dev@illwieckz.net>
+# VERSION: 20140222
+# License: Expat
+
+version='0.4.1'
+project='epsonepl'
+driver='epsoneplijs'
+workspace='workspace'
+dir="${driver}-${version}"
+archive="${dir}.tgz"
+
+cd "${workspace}"
+
+wget -nd -c "http://sourceforge.net/projects/${project}/files/${project}/${version}/${archive}/download" -O "${archive}"
+tar -xzf "${archive}"
+
+cd "${dir}"
+./configure --prefix='/usr'
+make
+
+cat > install.sh <<\EOF
+make install
+cd foomatic_scripts/
+./install_debian
+EOF
+
+chmod +x install.sh
+fakeroot checkinstall --default --pkgname="${driver}" --pkgversion="${version}" --pkglicense 'unknown' --fstrans --install='no' make install
+cp -a "${driver}_"${version}"-1_"*".deb" ..
+
+#EOF
